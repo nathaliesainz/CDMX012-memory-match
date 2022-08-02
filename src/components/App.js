c9e6fa496ee242c5b38336c034842e1ca40e8b69
nathaliesainz <nathaliesainz13@gmail.com>
2022-08-02 18:32:23 -0500

    Cleaned code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,59 @@
-//function to print/show data on screen
-export function deckOnScreen(cards){
-    return `<div class="cardContainer">
-    <div class="innerCardPokemon">
-      <div class="frontCardPokemon">
-        <p><img src="${cards.image}"</p>
-        <h2>${cards.id}</h2>
-      </div>
-      <div class="backCardPokemon">
-       <img src="./img/backCard.png" style="width:100px;height:100px;">
-      </div>
-    </div>
-    </div>`
-  }
+//cards with both sides and its content
+let options = [];
+
+export function deckOnScreen(cards) {
+  let cardContainer = document.createElement("div");
+  cardContainer.className = "cardContainer";
+
+  let innerCard = document.createElement("div");
+  innerCard.className = "innerCard";
+
+  let frontCard = document.createElement("div");
+  frontCard.className = "frontCard";
+
+  let cardName = document.createElement("div");
+  cardName.textContent = cards.id;
+
+  let frontImage = document.createElement("img");
+  frontImage.className = "frontImage";
+  frontImage.src = cards.image;
+
+  let backCard = document.createElement("div");
+  backCard.className = "backCard";
+  backCard.setAttribute("name", cards.id);
+
+  let backImage = document.createElement("img");
+  backImage.className = "backImage";
+  backImage.src = "./img/backCard.png";
+
+  cardContainer.appendChild(innerCard);
+  innerCard.appendChild(frontCard);
+  frontCard.appendChild(frontImage);
+  frontCard.appendChild(cardName);
+  innerCard.appendChild(backCard);
+  backCard.appendChild(backImage);
+
+
+  cardContainer.addEventListener("click", () => {
+    backCard.style.display = "none";
+    options.push(backCard);
+
+    if (options.length === 2) {
+      if (options[0].getAttribute("name") === options[1].getAttribute("name")) {
+        console.log("match");
+        //frontCard.style.border = "solid green 2px";
+      } else {
+        console.log(
+          options[0].getAttribute("name"),
+          options[1].getAttribute("name")
+        );
+        options.forEach((cards) => {
+          setTimeout(() => (cards.style.display = ""), 1000);
+        });
+      }
+      options = [];
+    }
+  });
+
+  return cardContainer;
+}
--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ import data from './data/pokemon/pokemon.js'
 import { deckOnScreen } from './components/App.js';
 
 //declaración de variables para interactuar con el dom y la database
-const nodoRoot = document.getElementById("root")
+const nodoRoot = document.getElementById('root')
 const firstSet = Object.values(data.items);
 const secondSet = Object.values(data.items);
+const sectionCards = document.createElement('div')
+sectionCards.classList='sectionCards'
 let finalSet = firstSet.concat(secondSet);
 finalSet = finalSet.sort(() => Math.random() - 0.5);
-let deck=""
 
 //ciclo con el que iteramos cada uno de los pokemones
 finalSet.forEach(pokemon => {
-  deck+=deckOnScreen(pokemon)
+  sectionCards.appendChild(deckOnScreen(pokemon))
 });
 
 // printing deck on HTML and showing it on screen
-nodoRoot.innerHTML=deck
-// console.log(deck)
+nodoRoot.appendChild(sectionCards)
